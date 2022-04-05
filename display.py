92a32bdef54ef984df302ecea6ed79e7a7957bed
ddoberne <ddoberne@gmail.com>
2022-04-04 17:21:40 -0700

    Add files via upload

--- display.py
+++ display.py
@@ -23,26 +23,20 @@ st.write('# The Filthiest')
 st.write('The top 5 ' + pitch_type + 's from MLB games on ' + date)
 
 
-# In[21]:
+# In[25]:
 
 
 df = pd.read_csv(filename, index_col = 0)
 leaderboard = df.loc[(df.pitch_type == pitch_type) & (df.result == 'Swinging Strike')].sort_values(by = 'rpm', ascending = False).head()
 leader = leaderboard.iloc[0]
 leaderboard_show = leaderboard[['pitcher', 'batter', 'inning', 'mph', 'rpm', 'vbreak', 'hbreak']]
-leaderboard_show.columns = ['Pitcher', 'Batter', 'Inning', 'Velocity (mph)', 'RPM', 'VBreak', 'HBreak']
+leaderboard_show.columns = ['Pitcher', 'Batter', 'Inning', 'Velo (mph)', 'RPM', 'VBreak', 'HBreak']
 st.dataframe(leaderboard_show)
 
 
 # In[22]:
 
 
-st.write(f"{leader.pitcher}'s {pitch_type.lower()} to {leader.batter} in inning {str(leader.inning)}, {leader['count'][0]}-{leader['count'][1]} count.")
-st.write('[Watch on MLB Film Room](https://www.mlb.com/video/search?q=' + leader.pitcher.replace(' ', '+') + '+' +     leader.batter.replace(' ', '+') + '+inning+' + str(leader.inning) + '+' + str(leader['count'][0]) + '+ball+' +     str(leader['count'][1]) + '+strike&qt=FREETEXT)')
-
-
-# In[ ]:
-
-
-
+st.write(f"{leader.pitcher}'s {pitch_type.lower()} to {leader.batter} in inning {str(leader.inning)}, {leader['count'][1]}-{leader['count'][3]} count.")
+st.write('[Watch on MLB Film Room](https://www.mlb.com/video/search?q=' + leader.pitcher.replace(' ', '+') + '+' +     leader.batter.replace(' ', '+') + '+inning+' + str(leader.inning) + '+' + str(leader['count'][1]) + '+ball+' +     str(leader['count'][3]) + '+strike&qt=FREETEXT)')
 
