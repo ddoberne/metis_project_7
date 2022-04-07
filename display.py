f33f2fed14169a72962cb405095a11883dea1ed1
ddoberne <ddoberne@gmail.com>
2022-04-07 09:56:56 -0700

    Update display.py

--- display.py
+++ display.py
@@ -100,7 +100,7 @@ if len(leaderboard) > 0:
         ax = sns.kdeplot(x = df['fifax'])
         ax.set_xlabel('FiFaX')
         ax.annotate(f"{leader.pitcher}'s {leader.pitch_type_raw}", xy = (leader[sort], 0), xytext = (leader[sort], 0.02),
-                    arrowprops = dict(color = 'red'), horizontalalignment = 'middle')
+                    arrowprops = dict(color = 'red'), horizontalalignment = 'center')
         ax.set(title = f'Distribution of {sort_in} for {pitch_type_in}s on {date}')
         st.pyplot(fig)
         
