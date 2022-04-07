75e22e1a2902a170f793b90aede589e2b057bd59
ddoberne <ddoberne@gmail.com>
2022-04-06 23:03:18 -0700

    Add files via upload

--- display.py
+++ display.py
@@ -72,7 +72,7 @@ is_ascending = False
 
 leaderboard = df.loc[(df.pitch_type == pitch_type)].sort_values(by = sort, ascending = is_ascending)
 if pitcher_search != '':
-    leaderboard = leaderboard.loc[leaderboard['pitcher'].apply(lambda pitcher_name: pitcher_search in pitcher_name)]
+    leaderboard = leaderboard.loc[leaderboard['pitcher'].apply(lambda pitcher_name: pitcher_search.lower() in pitcher_name.lower())]
 if len(leaderboard) > 0:
     show_n = min(len(leaderboard), 5)
     leader = leaderboard.iloc[leader_index - 1]
