648dc400a395695bab30c21ceecec13ed5059f44
ddoberne <ddoberne@gmail.com>
2022-04-06 18:01:55 -0700

    Add files via upload

--- display.py
+++ display.py
@@ -8,30 +8,69 @@ import pandas as pd
 import streamlit as st
 
 
-# In[19]:
+# In[ ]:
+
+
+st.write('# The Filthiest')
+st.write('**The Filthiest** reads in Statcast data from Baseball Savant and calculates the filthiest pitches thrown each day.')
+st.write("**FiFaX**, or **Filth Factor X** is the probability a pitch will be a swinging strike, called strike, or foul tip given that the pitch is not a ball and doesn't result in a foul ball.")
+st.write('Predictions are given by a Random Forest model trained on all pitches thrown in 2021.')
+
+
+# In[ ]:
 
 
 date = '2021-9-1'
-filename = '9/' + date
-pitch_type = st.selectbox('Pitch type:', ('4-Seam Fastball', 'Slider', 'Curveball'))
+filename = date + '.csv'
+df = pd.read_csv(filename, index_col = 0)
+df = df.loc[df['result'] == 'Strike']
+
+
+# In[19]:
+
+
+
+
+
+
+pitch_type = st.selectbox('Pitch type:', ('4-Seam Fastball', 'Slider', '2-Seam Fastball/Sinker', 'Changeup', 'Curveball', 'Splitter/Knuckleball', 'Cutter'))
+
+sort = st.selectbox('Sort by:', ('FiFaX', 'MPH', 'RPM', 'VBreak', 'HBreak'))
+
 leader_index = st.selectbox('Select index:', (1,2,3,4,5))
+pitch_dict = {'4-Seam Fastball': 'Fastball',
+              'Slider': 'Slider',
+              '2-Seam Fastball/Sinker': 'Sinker',
+              'Changeup': 'Changeup',
+              'Curveball': 'Curveball',
+              'Splitter/Knuckleball': 'Splitter',
+              'Cutter'}
+sort_dict = {'FiFaX': 'fifax',
+             'MPH': 'mph',
+             'RPM': 'rpm',
+             'VBreak': 'vbreak',
+             'HBreak': 'hbreak'}
 
 
 # In[20]:
 
 
-st.write('# The Filthiest')
-st.write('The top 5 ' + pitch_type + 's from MLB games on ' + date)
+
+st.write('The top 5 ' + pitch_type + 's from MLB games on ' + date + ', sorted by ' + sort)
+
+pitch_type = pitch_dict[pitch_type]
+sort = sort_dict[sort]
 
 
 # In[25]:
 
 
-df = pd.read_csv(filename, index_col = 0)
-leaderboard = df.loc[(df.pitch_type == pitch_type) & (df.result == 'Swinging Strike')].sort_values(by = 'rpm', ascending = False).head()
+is_ascending = False
+
+leaderboard = df.loc[(df.pitch_type == pitch_type)].sort_values(by = sort, ascending = is_ascending).head()
 leader = leaderboard.iloc[leader_index - 1]
-leaderboard_show = leaderboard[['pitcher', 'batter', 'inning', 'mph', 'rpm', 'vbreak', 'hbreak']]
-leaderboard_show.columns = ['Pitcher', 'Batter', 'Inning', 'Velo (mph)', 'RPM', 'VBreak', 'HBreak']
+leaderboard_show = leaderboard[['pitcher', 'batter', 'inning', 'mph', 'rpm', 'vbreak', 'hbreak', 'fifax']]
+leaderboard_show.columns = ['Pitcher', 'Batter', 'Inning', 'Velo (mph)', 'RPM', 'VBreak', 'HBreak', 'FiFaX']
 st.dataframe(leaderboard_show)
 
 
