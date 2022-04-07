8b61ed6d5ce338d57a85718895a28bb0d274f6cf
ddoberne <ddoberne@gmail.com>
2022-04-07 10:01:52 -0700

    Add files via upload

--- display.py
+++ display.py
@@ -97,9 +97,9 @@ if len(leaderboard) > 0:
         
         fig = plt.figure(figsize = (12,4))
         sns.set_theme('notebook')
-        ax = sns.kdeplot(x = df['fifax'])
-        ax.set_xlabel('FiFaX')
-        ax.annotate(f"{leader.pitcher}'s {leader.pitch_type_raw}", xy = (leader[sort], 0), xytext = (leader[sort], 0.02),
+        ax = sns.kdeplot(x = df[sort])
+        ax.set_xlabel(sort_in)
+        ax.annotate(f"{leader.pitcher}'s {leader.pitch_type_raw}", xy = (leader[sort], 0), xytext = (leader[sort], 0.2),
                     arrowprops = dict(color = 'red'), horizontalalignment = 'center')
         ax.set(title = f'Distribution of {sort_in} for {pitch_type_in}s on {date}')
         st.pyplot(fig)
