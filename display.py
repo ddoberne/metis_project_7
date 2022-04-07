29af86aea0c1c292511b73b811da1dfa8a8f4597
ddoberne <ddoberne@gmail.com>
2022-04-07 09:33:56 -0700

    Add files via upload

--- display.py
+++ display.py
@@ -6,6 +6,7 @@
 
 import pandas as pd
 import streamlit as st
+import seaborn as sns
 
 
 # In[ ]:
@@ -21,26 +22,27 @@ st.write('# The Filthiest')
 date = '2021-9-1'
 filename = date + '.csv'
 df = pd.read_csv(filename, index_col = 0)
-df = df.loc[df['result'] == 'Strike']
-df['mph'] = df['mph'].round(decimals = 2)
-df['fifax'] = df['fifax'].round(decimals = 3)
+df['fifax'] = df['fifax'] * 100
+pd.options.display.float_format = "{:,.2f}".format
 
 
 # In[19]:
 
 
-pitch_type = st.sidebar.selectbox('Pitch type:', ('4-Seam Fastball', 'Slider', '2-Seam Fastball/Sinker', 'Changeup', 'Curveball', 'Splitter/Knuckleball', 'Cutter'))
+pitch_type_in = st.sidebar.selectbox('Pitch type:', ('4-Seam Fastball', 'Slider', '2-Seam Fastball/Sinker', 'Changeup', 'Curveball', 'Splitter/Knuckleball', 'Cutter'))
 
-sort = st.sidebar.selectbox('Sort by:', ('FiFaX', 'MPH', 'RPM', 'VBreak', 'HBreak'))
+sort_in = st.sidebar.selectbox('Sort by:', ('FiFaX', 'MPH', 'RPM', 'VBreak', 'HBreak'))
 
 leader_index = st.sidebar.selectbox('Select index:', (1,2,3,4,5))
 pitcher_search = st.sidebar.text_input('Pitcher search:', value = '')
+hitter_wins = st.sidebar.checkbox('Show hitter victories instead')
 
 
 st.sidebar.write('**The Filthiest** reads in Statcast data from Baseball Savant and calculates the filthiest pitches thrown each day.')
 st.sidebar.write("**FiFaX**, or **Filth Factor eXpected**, is the probability a pitch will be a swinging strike, called strike, or foul tip, given that the pitch is a strike or put in play.")
 st.sidebar.write('Predictions are given by a Random Forest model trained on all pitches thrown in 2021.')
 st.sidebar.write('Created by Dayv Doberne | [Twitter](https://www.twitter.com/Sunyveil_Sports)')
+st.sidebar.write('Inspired by [Pitching Ninja](https://twitter.com/PitchingNinja)')
 
 
 pitch_dict = {'4-Seam Fastball': 'Fastball',
@@ -61,12 +63,17 @@ sort_dict = {'FiFaX': 'fifax',
 
 
 
-pitch_type = pitch_dict[pitch_type]
-sort = sort_dict[sort]
+pitch_type = pitch_dict[pitch_type_in]
+sort = sort_dict[sort_in]
 
 
 # In[25]:
 
+
+if hitter_wins:
+    df = df.loc[(df['result_raw'] == 'In play, no out') | (df['result_raw'] == 'In play, run(s)')]
+else:
+    df = df.loc[df['result'] == 'Strike']
 
 is_ascending = False
 
@@ -80,12 +87,23 @@ if len(leaderboard) > 0:
     leaderboard_show = leaderboard[['pitcher', 'batter', 'mph', 'rpm', 'vbreak', 'hbreak', 'fifax']]
     leaderboard_show.columns = ['Pitcher', 'Batter', 'Velo (mph)', 'RPM', 'VBreak', 'HBreak', 'FiFaX']
     leaderboard_show.index = range(1, len(leaderboard_show) + 1)
-    st.write(f'The top {str(show_n)} {pitch_type}s from MLB games on {date}, sorted by {sort}.')
+    st.write(f'The top {str(show_n)} {pitch_type_in}s from MLB games on {date}, sorted by {sort_in}.')
     st.dataframe(leaderboard_show.head(show_n))
     
     if leader_index <= show_n:
         st.write(f"{leader.pitcher}'s {pitch_type.lower()} to {leader.batter} in inning {str(leader.inning)}, {leader['count'][1]}-{leader['count'][4]} count.")
         st.components.v1.iframe(f"https://www.mlb.com/video/search?q={leader.pitcher.replace(' ', '+')}+            {leader.batter.replace(' ', '+')}+inning+{str(leader.inning)}+{str(leader['count'][1])}+ball+            {str(leader['count'][4])}+strike&qt=FREETEXT", height = 600)
+        
+        
+        sns.set_theme('notebook')
+        ax = sns.kdeplot(x = df['fifax'])
+        ax.set_xlabel('FiFaX')
+        ax.annotate(f"{leader.pitcher}'s {leader.pitch_type_raw}", xy = (leader[sort], 0), xytext = (leader[sort], 2), arrowprops = dict(color = 'red'))
+        ax.set(title = f'Distribution of {sort_in} for {pitch_type_in}s on {date}')
+        st.pyplot(ax)
+        
+    else:
+        st.write('Index out of range!')
 else:
     st.write('Player not found!')
 
